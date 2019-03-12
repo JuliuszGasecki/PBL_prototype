eb1d41f28db2ac00eadecaf261c35029cfd396eb
Juliusz Gąsecki <julekg.97@gmail.com>
2019-03-12 16:25:35 +0100

    Pick up block (destroy object)

---
+++ PBL_prototype/Assets/Scripts/Package/PickUpTarget.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpTarget : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Girl" || other.tag == "Boi")
+        {
+            Debug.Log("Podnoszem skrzynkę");
+            if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Alpha9))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
