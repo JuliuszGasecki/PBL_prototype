0a69b0daa60f7fb8842fc57da40131f75098bc30
Mateusz Budzyński <mattibu@gmail.com>
2019-03-20 19:41:09 +0100

    Added destroying for boy

---
+++ PBL_prototype/Assets/Scripts/Destroying/Destroy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+       
+        if (other.tag == "Boi" )
+        {           
+            if (Input.GetButtonDown("Fire1"))
+            {
+                IDestroyable destroyable = gameObject.GetComponent<IDestroyable>();
+                if (destroyable != null)
+                {
+                    destroyable.Destroy();
+                }
+            }
+            
+        }
+        
+    }
+}
