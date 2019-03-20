2f19c61be993fd7031fcd7103f6b53469db6913e
Bartosz <bartekstep@gmail.com>
2019-03-20 23:02:35 +0100

    Minimap added, small refactoring, fixed bugs

---
+++ PBL_prototype/Assets/Scripts/RadioMinimap/ShowEnemies.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowEnemies : MonoBehaviour
+{
+    private bool _isHacked;
+    private GameObject[]_enemiesIcons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemiesIcons = GameObject.FindGameObjectsWithTag("EnemyIcon");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay(Collider collision)
+    {
+        string tag = collision.gameObject.tag;
+        if (Input.GetKeyDown(KeyCode.E) && tag.Equals("Girl") && !_isHacked)
+        {
+            _isHacked = true;
+        }
+
+        if (_isHacked && tag.Equals("Girl"))
+        {
+            foreach (GameObject i in _enemiesIcons)
+            {
+                i.layer = LayerMask.NameToLayer("Minimap");
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        string tag = collision.gameObject.tag;
+        if (tag.Equals("Girl"))
+        {
+            foreach (GameObject i in _enemiesIcons)
+            {
+                i.layer = LayerMask.NameToLayer("Hiden");
+            }
+        }
+    }
+}
