856a0cae5deb8c6e9884a09622daaf2fcf25d816
Juliusz Gąsecki <julekg.97@gmail.com>
2019-03-23 20:23:57 +0100

    Street prefabs (redline on map) / bugged object transparent

---
+++ PBL_prototype/Assets/Scripts/Camera/CameraChangeMaterial.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChangeMaterial : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform[] heroes;
+    [SerializeField]
+    private List<RaycastHit> detectedObjects;
+    [SerializeField]
+    private Material transparentMaterial;
+
+    private void Start()
+    {
+        detectedObjects = new List<RaycastHit>();
+    }
+
+    private void Update()
+    {
+        CheckRays();
+    }
+
+    private void CheckRays()
+    {
+        RaycastHit[] girlsHits;
+        RaycastHit[] boizHits;
+        Debug.DrawLine(heroes[0].position, transform.position, Color.blue, 0.1f);
+        Debug.DrawLine(heroes[1].position, transform.position, Color.blue, 0.1f);
+        girlsHits = Physics.RaycastAll(heroes[0].position, transform.position);
+        boizHits = Physics.RaycastAll(heroes[1].position, transform.position);
+        setOldMeterialToObjects(girlsHits);
+        setOldMeterialToObjects(boizHits);
+        foreach(RaycastHit hit in girlsHits)
+        {
+        if (hit.transform.GetComponent<MaterialChanger>() != null)
+            {
+                hit.transform.GetComponent<MaterialChanger>().UseNewMaterial(transparentMaterial);
+                detectedObjects.Add(hit);
+            }
+
+        }
+
+        foreach (RaycastHit hit in boizHits)
+        {
+            if (hit.transform.GetComponent<MaterialChanger>() != null)
+            {
+                hit.transform.GetComponent<MaterialChanger>().UseNewMaterial(transparentMaterial);
+                detectedObjects.Add(hit);
+            }
+
+        }
+
+    }
+
+    private void setOldMeterialToObjects(RaycastHit[] hits)
+    {
+        List<RaycastHit> elementsToRemove = new List<RaycastHit>();
+        if(detectedObjects != null)
+        {
+            foreach (RaycastHit hit in detectedObjects)
+            {
+                bool isInHits = false;
+                for(int i = 0; i < hits.Length; i++)
+                {
+                    if (hit.transform.position.Equals(hits[i].transform.position))
+                    {
+                        isInHits = true;
+                    }
+                }
+                if (!isInHits)
+                {
+                    Debug.Log(hit.transform.position);
+                    elementsToRemove.Add(hit);
+                }
+            }
+            foreach(RaycastHit elementToRemove in elementsToRemove)
+            {
+                elementToRemove.transform.GetComponent<MaterialChanger>().UseOldMaterial();
+                detectedObjects.Remove(elementToRemove);
+
+            }
+        }  
+    }
+}
---
+++ PBL_prototype/Assets/Scripts/Camera/MaterialChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChanger : MonoBehaviour
+{
+    [SerializeField]
+    private Material myMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMaterial = this.GetComponent<Renderer>().material;
+    }
+
+    public void UseNewMaterial(Material m)
+    {
+        this.GetComponent<Renderer>().material = m;
+    }
+
+    public void UseOldMaterial()
+    {
+        Debug.Log("Ustawiam stary materiał");
+        this.GetComponent<Renderer>().material = myMaterial;
+    }
+}
