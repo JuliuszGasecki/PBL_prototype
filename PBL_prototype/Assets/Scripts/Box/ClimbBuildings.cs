b773260786b7e994c815e9ba01d6b5ab6848950e
Juliusz Gąsecki <julekg.97@gmail.com>
2019-03-25 01:45:20 +0100

    Climbing

---
+++ PBL_prototype/Assets/Scripts/Box/ClimbBuildings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbBuildings : MonoBehaviour
+{
+    private GameObject objectToclimbOn;
+    private bool isClimbing = false;
+    private GameObject girlGameObject = GameObject.Find("Girl");
+    [SerializeField]
+    private float climbSpeed = 0.06f;
+    private float accuracyOfClimb = 0.03f;
+    private float pointPositionY;
+    private float colliderYPositionChangerValue = 0.3f;
+}
